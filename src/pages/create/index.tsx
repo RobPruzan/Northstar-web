91db68247cdb3475a8b9348b92b5bda812ba8ce7
RobPruzan <rapruzan@buffalo.edu>
2023-03-20 19:14:59 -0400

    Add search functionality

--- src/pages/create/index.tsx
+++ src/pages/create/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { useContext, useState } from "react";
 import DocumentSelections from "~/components/ChooseDocument/DocumentSelections";
 import Library from "~/components/ChooseDocument/Library";
@@ -5,50 +6,50 @@ import UserDocuments from "~/components/ChooseDocument/UserDocuments";
 import CreateControlPanel from "~/components/ControlPanel/CreateControlPanel";
 import CreateModal from "~/components/CreateDocument/CreateModal";
 import NavBar from "~/components/NavBar";
+import { QueryContext } from "~/Context/QueryContext";
 import { SelectedDocumentsContext } from "~/Context/SelectedDocumentsContext";
 
 const index = () => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const { selectedDocuments, setSelectedDocuments } = useContext(
-    SelectedDocumentsContext
-  );
-  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { setSelectedDocuments } = useContext(SelectedDocumentsContext);
+
   const [collectionTypeToView, setCollectionTypeToView] = useState<
     "user" | "library"
   >();
-  return (
-    <div className="flex h-screen w-screen flex-col overflow-hidden ">
-      <NavBar />
-      {/* <div className="flex h-full  flex-col items-center justify-center  overflow-hidden"> */}
-      <DocumentSelections
-        selectedDocuments={selectedDocuments}
-        setSelectedDocuments={setSelectedDocuments}
-      />
-      <div className="flex h-4/5 w-full overflow-hidden">
-        <div className="flex h-full w-2/12 border border-l-0 border-t-0 border-slate-700 shadow-2xl">
-          <CreateControlPanel
-            setCollectionTypeToView={setCollectionTypeToView}
-            collectionTypeToView={collectionTypeToView}
-          />
-        </div>
 
-        <div className="flex h-full w-7/12 flex-wrap  ">
-          <Library
-            collectionTypeToView={collectionTypeToView}
-            setCollectionTypeToView={setCollectionTypeToView}
-          />
-        </div>
-        <div className="flex    h-full w-3/12  flex-col items-center overflow-y-scroll  border  border-r-0 border-t-0 border-slate-700 p-2 px-2 shadow-2xl ">
-          {/* <section > */}
-          <div className="w-full items-center justify-center border-b-2 border-slate-700">
-            <CreateModal />
+  const [searchName, setSearchName] = useState<string>();
+  return (
+    <QueryContext.Provider value={{ searchName, setSearchName }}>
+      <div className="flex h-screen w-screen flex-col overflow-hidden ">
+        <NavBar />
+        {/* <div className="flex h-full  flex-col items-center justify-center  overflow-hidden"> */}
+        <DocumentSelections />
+        <div className="flex h-4/5 w-full overflow-hidden">
+          <div className="flex h-full w-2/12 border border-l-0 border-t-0 border-slate-700 shadow-2xl">
+            <CreateControlPanel
+
+              collectionTypeToView={collectionTypeToView}
+              setCollectionTypeToView={setCollectionTypeToView}
+            />
           </div>
 
-          <UserDocuments setSelectedDocuments={setSelectedDocuments} />
-          {/* </section> */}
+          <div className="flex h-full w-7/12 flex-wrap  ">
+            <Library
+              collectionTypeToView={collectionTypeToView}
+              setCollectionTypeToView={setCollectionTypeToView}
+            />
+          </div>
+          <div className="flex    h-full w-3/12  flex-col items-center overflow-y-scroll  border  border-r-0 border-t-0 border-slate-700 p-2 px-2 shadow-2xl ">
+            {/* <section > */}
+            <div className="w-full items-center justify-center border-b-2 border-slate-700">
+              <CreateModal />
+            </div>
+
+            <UserDocuments setSelectedDocuments={setSelectedDocuments} />
+            {/* </section> */}
+          </div>
         </div>
       </div>
-    </div>
+    </QueryContext.Provider>
     // </div>
   );
 };
